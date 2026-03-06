375ffd49c5ff9add3a1f57cdacdd3934be7b0be2
ivn-ln <illarn@ya.ru>
2026-03-06 18:04:33 +0300

    stringifier: added JSON formatting support

--- illarn/stringifier.lua
+++ illarn/stringifier.lua
@@ -1,25 +1,73 @@
 local t_insert, t_concat = table.insert, table.concat
 local s_format, s_rep = string.format, string.rep
 
+---@alias StringifierFormatConfig {start: string, end: string, delimeter: string, array_start: string, array_end: string, kv_start: string, kv_end: string, kv_delimeter: string, array_value_format: string, key_format: string, value_format: string, newline: string, space: string}
+
 ---@class Stringifier
 ---@field compact boolean
 ---@field tab_size number
+---@field format StringifierFormat
+---@field Format table<string, number>
+---@field private _s_format_configs table<StringifierFormat, StringifierFormatConfig>
 local M = {}
 M.mt = {
 	__index = M
 }
 
-function M:spaces(n)
-	return s_rep(" ", n * self.tab_size)
+function M:_spaces(n, format_config)
+	return s_rep(format_config.space, n * (self.tab_size or 2))
 end
+
+---@enum StringifierFormat
+M.Format = {
+	DEFAULT = 1,
+	JSON = 2,
+	YAML = 3,
+	TOML = 4
+}
+
+M._s_format_configs = {
+	[M.Format.DEFAULT] = {
+		["start"] = "<table: ",
+		["end"] = ">",
+		["delimeter"] = ", ",
+		["array_start"] = "[",
+		["array_end"] = "]",
+		["kv_start"] = "{",
+		["kv_end"] = "}",
+		["kv_delimeter"] = ": ",
+		["key_format"] = "%s",
+		["value_format"] = "%s",
+		["array_value_format"] = "%s",
+		["space"] = " ",
+		["newline"] = "\n",
+	},
+	[M.Format.JSON] = {
+		["start"] = "{",
+		["end"] = "}",
+		["delimeter"] = ", ",
+		["array_start"] = "\"array_part\": [",
+		["array_end"] = "]",
+		["kv_start"] = "\"kv_part\": {",
+		["kv_end"] = "}",
+		["kv_delimeter"] = ": ",
+		["key_format"] = "\"%s\"",
+		["value_format"] = "\"%s\"",
+		["array_value_format"] = "\"%s\"",
+		["space"] = " ",
+		["newline"] = "\n",
+	},
+}
 
 ---@param compact? boolean if true will return tables on one line. default false
 ---@param tab_size? number indentation size in spaces. default 2
+---@param format? StringifierFormat formatting of the result (tables only)
 ---@return table
-function M.new(compact, tab_size)
+function M.new(compact, tab_size, format)
 	local t = setmetatable({
 		compact = compact or false,
-		tab_size = tab_size or 2
+		tab_size = tab_size or 2,
+		format = format or M.Format.DEFAULT
 	}, M.mt)
 
 	return t
@@ -55,8 +103,11 @@ function M:get(obj, _seen, _indent, _current_indent)
 			return s_format("<userdata: %p>", obj)
 		end
 	elseif obj_type == "table" then
+		local compact = self.compact
+
+		local format_config = M._s_format_configs[self.format] or M._s_format_configs[M.Format.DEFAULT]
 		if _seen[obj] then
-			return "<circular reference>"
+			return "!circular reference!"
 		end
 
 		_seen[obj] = true
@@ -68,7 +119,12 @@ function M:get(obj, _seen, _indent, _current_indent)
 			return str
 		end
 
-		local result = {"<table: "}
+		local result = {format_config.start}
+		if not compact then
+			t_insert(result, format_config.newline)
+			_current_indent = _current_indent + 1
+			t_insert(result, self:_spaces(_current_indent, format_config))
+		end
 		local first = true
 
 		local max_index = 0
@@ -82,50 +138,52 @@ function M:get(obj, _seen, _indent, _current_indent)
 		end
 
 		if is_array and max_index == #obj then
-			-- array format
-			t_insert(result, "[")
+			t_insert(result, format_config.array_start)
 			for i = 1, max_index do
 				if not first then
-					t_insert(result, ",")
+					t_insert(result, format_config.delimeter)
 				else
 					first = false
 				end
-				if not self.compact then
-					t_insert(result, "\n")
-					t_insert(result, self:spaces(_current_indent + 1))
+				if not compact then
+					t_insert(result, format_config.newline)
+					t_insert(result, self:_spaces(_current_indent + 1, format_config))
 				end
-				t_insert(result, self:get(obj[i], _seen, _indent, _current_indent + 1))
+				t_insert(result, s_format(format_config.array_value_format, self:get(obj[i], _seen, _indent, _current_indent + 1)))
 			end
-			if max_index > 0 and not self.compact then
-				t_insert(result, "\n")
-				t_insert(result, self:spaces(_current_indent))
+			if max_index > 0 and not compact then
+				t_insert(result, format_config.newline)
+				t_insert(result, self:_spaces(_current_indent, format_config))
 			end
-			t_insert(result, "]")
+			t_insert(result, format_config.array_end)
 		else
-			t_insert(result, "{")
+			t_insert(result, format_config.kv_start)
 			for k, v in pairs(obj) do
 				if not first then
-					t_insert(result, ",")
+					t_insert(result, format_config.delimeter)
 				else
 					first = false
 				end
-				if not self.compact then
-					t_insert(result, "\n")
-					t_insert(result, self:spaces(_current_indent + 1))
+				if not compact then
+					t_insert(result, format_config.newline)
+					t_insert(result, self:_spaces(_current_indent + 1, format_config))
 				end
-				t_insert(result, self:get(k, _seen, _indent, _current_indent + 1))
-				t_insert(result, ": ")
-				t_insert(result, self:get(v, _seen, _indent, _current_indent + 1))
+				t_insert(result, s_format(format_config.key_format, self:get(k, _seen, _indent, _current_indent + 1)))
+				t_insert(result, format_config.kv_delimeter)
+				t_insert(result, s_format(format_config.value_format, self:get(v, _seen, _indent, _current_indent + 1)))
 			end
-			if next(obj) ~= nil and not self.compact then
-				t_insert(result, "\n")
-				t_insert(result, self:spaces(_current_indent))
+			if next(obj) ~= nil and not compact then
+				t_insert(result, format_config.newline)
+				t_insert(result, self:_spaces(_current_indent, format_config))
 			end
-			t_insert(result, "}")
+			t_insert(result, format_config.kv_end)
 		end
 
 		_seen[obj] = nil
-		t_insert(result, ">")
+		if not compact then
+			t_insert(result, format_config.newline)
+		end
+		t_insert(result, format_config["end"])
 		return t_concat(result)
 	else
 		return tostring(obj)
